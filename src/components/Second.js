fc6fcb5898f0c0da66aa29a8261ba359e73813cb
cagoncil <crystallophobia@gmail.com>
2022-06-19 00:36:27 -0700

    Add card component

--- src/components/Second.js
+++ src/components/Second.js
@@ -1,9 +1,12 @@
 import classes from './Second.module.css';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
+import React, { useState } from 'react';
 
 
 const Second = () => {
+  const [altitude, setAltitude] = useState(10);
+
   return (
     <div className={classes.second}>
       <h2>Testimonials</h2>
@@ -13,18 +16,21 @@ const Second = () => {
           {[0, 1, 2].map((value) => (
             <Grid key={value} item>
               <Paper
+                onMouseEnter={() => setAltitude(3)}
+                onMouseLeave={() => setAltitude(10)}
                 sx={{
                   height: 270,
                   width: 200,
                   backgroundColor: (theme) =>
                     theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
                 }}
-                elevation={10} 
-              />
+                elevation={altitude} 
+              >
+              </Paper>
             </Grid>
           ))}
         </Grid>
-        
+
     </div>
   )
 }
