62cbbfb5c3d86869964febab8821e4411e0d9270
cagoncil <crystallophobia@gmail.com>
2022-06-18 23:52:11 -0700

    modify component styles

--- src/components/Second.js
+++ src/components/Second.js
@@ -1,10 +1,30 @@
 import classes from './Second.module.css';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+
 
 const Second = () => {
   return (
     <div className={classes.second}>
-        <h2>hello</h2>
-        yes hi
+      <h2>Testimonials</h2>
+
+      <Grid sx={{ flexGrow: 1 }} container spacing={2}></Grid>
+        <Grid container justifyContent="center" spacing={2}>
+          {[0, 1, 2].map((value) => (
+            <Grid key={value} item>
+              <Paper
+                sx={{
+                  height: 270,
+                  width: 200,
+                  backgroundColor: (theme) =>
+                    theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+                }}
+                elevation={10} 
+              />
+            </Grid>
+          ))}
+        </Grid>
+        
     </div>
   )
 }
