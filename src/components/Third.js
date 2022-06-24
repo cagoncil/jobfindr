858d33de5ab37318b0204b36e4926c668994c807
cagoncil <crystallophobia@gmail.com>
2022-06-24 13:49:19 -0700

    change bottom page content

--- src/components/Third.js
+++ src/components/Third.js
@@ -3,8 +3,8 @@ import classes from './Third.module.css';
 const Third = () => {
   return (
     <div className={classes.third}>
-        <h2>Third!</h2>
-        Woohoo!
+        <h2>Stay in contact</h2>
+        Connect with your network here.
     </div>
   )
 }
